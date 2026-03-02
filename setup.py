d548c6c8e70104eac87cdac5b9246c1fa5f3a660
Craig Miller <5473482+craigmillernz@users.noreply.github.com>
2026-03-03 08:35:19 +1300

    remove un-needed setup files

--- setup.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026 Craig Miller.
-
-#!/usr/bin/env python
-"""Minimal setup.py for setuptools with pyproject.toml configuration."""
-
-from setuptools import setup
-
-if __name__ == "__main__":
-    setup()
